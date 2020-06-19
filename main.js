bed2df1ecaabe9233641d57fca17980b4bd38e11
Madhura P <madhura.pharande@somaiya.edu>
2020-06-19 16:01:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -29,3 +29,14 @@ function displayResults(weather){
 
 }
 
+function dateBuilder(d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days.[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonths()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
