39a81ccfef9c123fad90b5d19def8378a089e0a9
Madhura P <madhura.pharande@somaiya.edu>
2020-06-19 17:25:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -27,13 +27,21 @@ function displayResults(weather){
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
 
+    let temp = document.querySelector('.current .temp);
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.current .hi-low');
+    hilow.innerText = `${weather.main.temp_min}°c/ ${weather.main.temp_max}°c`;
 }
 
 function dateBuilder(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days.[d.getDay()];
+    let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonths()];
     let year = d.getFullYear();
