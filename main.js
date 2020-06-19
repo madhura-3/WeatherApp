dfa4a767f1a45c7259276f3e8d09e09cfb8ea0e8
Madhura P <madhura.pharande@somaiya.edu>
2020-06-19 15:35:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const api = {
-	key: "a6efcd55ff63d71186be2affc3dffe3d",
-	baseurl: "https://api.openweathermap.org/data/2.5/"	
+    key: "a6efcd55ff63d71186be2affc3dffe3d",
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
-searchbox.addEventListner('keypress', setQuery);
+searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
 	if (evt.keyCode == 13){
 		getResults(searchbox.value);
-		console.log(searchbox.value);
 	}
 }
 
-function getResults(query){
-	fetch('${api.base}weather?q=${query}&units=metric&APPID=${api.key}')
-	.then(weather => {
-		return weather.json();
-	}).then(displayResults);
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
 }
 
 function displayResults(weather){
-	console.log(weather);
+    console.log(weather);
+
 }
